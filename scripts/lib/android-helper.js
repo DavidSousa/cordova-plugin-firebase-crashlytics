1b4efcfbde15ae797e0169aadcaa6a75fcbec3bd
DavidSousa <davidbdsousa@gmail.com>
2018-01-12 16:42:35 +0000

    Initial commit

---
+++ scripts/lib/android-helper.js
@@ -0,0 +1,44 @@
+
+var fs = require("fs");
+var path = require("path");
+var utilities = require("./utilities");
+
+module.exports = {
+
+    addFabricBuildToolsGradle: function() {
+
+        var buildGradle = utilities.readBuildGradle();
+
+        buildGradle +=  [
+            "",
+            "// Fabric Cordova Plugin - Start Fabric Build Tools ",
+            "buildscript {",
+            "    repositories {",
+            "        maven { url 'https://maven.fabric.io/public' }",
+            "    }",
+            "    dependencies {",
+            "        classpath 'io.fabric.tools:gradle:1.+'",
+            "    }",
+            "}",
+            "",
+            "apply plugin: 'io.fabric'",
+			"dependencies {",
+			"	compile('com.crashlytics.sdk.android:crashlytics:2.7.1@aar') {",
+			"		transitive = true",
+			"	}",
+			"}",
+            "// Fabric Cordova Plugin - End Fabric Build Tools",
+        ].join("\n");
+
+        utilities.writeBuildGradle(buildGradle);
+    },
+
+    removeFabricBuildToolsFromGradle: function() {
+
+        var buildGradle = utilities.readBuildGradle();
+
+        buildGradle = buildGradle.replace(/\n\/\/ Fabric Cordova Plugin - Start Fabric Build Tools[\s\S]*\/\/ Fabric Cordova Plugin - End Fabric Build Tools/, "");
+
+        utilities.writeBuildGradle(buildGradle);
+    }
+};
